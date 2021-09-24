cad5f97c4bbaea14b8e1f25b670977af23364dee
Denis Perchenko <plachenko@gmail.com>
2021-09-24 04:09:03 -0400

    basic digital greenscreen implemetation with tracking

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,105 @@
 	let ctx:CanvasRenderingContext2D;
 	let video:HTMLVideoElement;
 	let target:HTMLElement;
+	let stepNum = 1;
+	let prevData;
+	let capImg;
 
 	onMount(()=>{
 		ctx = canvas.getContext('2d');
 		canvas.width = 400;
 		canvas.height = 300;
-		requestAnimationFrame(step);
+		step();
 	});
 
 	function step(){
-		ctx.drawImage(video, 140, 100, video.videoWidth, video.videoHeight, 0, 0, 3000, 3000);
-		requestAnimationFrame(step);
+		/* 
+			1) Grab the video image, 
+				- it's image data Object 
+				- specify the image data pixels
+		*/
+		ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, 400, 300);
+		let img = ctx.getImageData(0,0,canvas.width,canvas.height);
+		let data = img.data;
+
+		// 2) Check and set previous image data.
+		if(!prevData){
+			prevData = data;
+		}
+
+		if(!capImg){
+			capImg = data;
+		}
+
+		// 3) Run through image data pixels...
+		for(let c = 0; c < data.length; c += 4){
+			let pix = {
+				r: data[c + 0] - capImg[c+0],
+				g: data[c + 1] - capImg[c + 1],
+				b: data[c + 2] - capImg[c+2],
+				a: 255
+			}
+
+			/*
+			if(data[c+0] <= capImg.data[c+0]-10 && data[c+0]>=capImg.data[c+0]+10){
+				data[c + 0] = 125;
+			}else{
+				data[c + 0] = data[c + 0];
+			}
+
+			if(data[c+1] == capImg.data[c+1]){
+				data[c + 1] = 20;
+			}else{
+				data[c + 1] = data[c+1];
+			}
+			*/
+
+			//Red Channel.
+			if(data[c+0] >= prevData[c+0]){
+				data[c + 0] = pix.r;
+			}else{
+				data[c + 0] = 0;
+			}
+
+			//Green Channel.
+			if(data[c+1] >= prevData[c+1]){
+				data[c + 1] = pix.g;
+			}else{
+				data[c + 1] = 0;
+			}
+
+			//Blue Channel.
+			if(data[c+2] >= prevData[c+2]){
+				data[c + 2] = pix.b;
+			}else{
+				data[c + 2] = 0;
+			}
+
+			//Alpha Channel.
+			let thresh= 0;
+			if(pix.r > thresh && pix.g > thresh && pix.b > thresh){
+				data[c + 3] = pix.a;
+			}else{
+				data[c+3] = 100;
+			}
+		}
+
+		let imgd = new ImageData(data, 80*5);
+		ctx.putImageData(imgd, 0,0);
+
+		prevData = data;
+
+		setTimeout(()=>{
+			requestAnimationFrame(step);
+		}, 100);
+	}
+
+	function capture(){
+		setTimeout(()=>{
+			console.log('capture');
+			capImg = ctx.getImageData(0,0,canvas.width,canvas.height).data;
+			requestAnimationFrame(step);
+		}, 1000);
 	}
 
 	navigator.mediaDevices.getUserMedia({video: true, audio: false}).
@@ -31,6 +119,7 @@
 <main>
 	<canvas bind:this={canvas} />
 	<div id="container">
+		<!--component
 		<div bind:this={target} class="target"></div>
 		<!--
 		{#each targetArr as target }
@@ -42,6 +131,7 @@
 
 		<video autoplay bind:this={video} />
 	</div>
+	<a href="#" on:click={capture}>capture</a>
 </main>
 
 <style>
@@ -59,12 +149,11 @@
 
 	canvas{
 		position: absolute;
-		top: 310px;
-		left: px;
 		border: 1px solid;
 	}
 
 	video{
+		display: none;
 		width: 400px;
 		position: absolute;
 		top:0px;
@@ -72,7 +161,7 @@
 	}
 
 	.target{
-		color:#F00;
+		background-color: #F00;
 		width: 100px;
 		height: 100px;
 		position: absolute;
@@ -85,5 +174,6 @@
 		height: 300px;
 		position: relative;
 		border:1px solid;
+
 	}
 </style>
